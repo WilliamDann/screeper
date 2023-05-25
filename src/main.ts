b8002044e603eb8429cd2c188639cff8752ed6de
WilliamDann <s-william.dann@lwtech.edu>
2023-05-25 13:41:33 -0700

    added findWorkRole

--- src/main.ts
+++ src/main.ts
@@ -1,53 +1,7 @@
-import RoomMediators from "./mediators/RoomMediators";
-import RoomMediator from "./mediators/RoomMediators";
 import roles        from "./roles/all";
-import HarvestSite  from "../old/HarvestSite";
-import SpawnSite    from "../old/SpawnSite";
-
-function poll(roomName: string)
-{
-    const room    = Game.rooms[roomName];
-    const inst    = RoomMediator.getInstance(roomName);
-    const siteMap = {};
-
-    // create harvest sites
-    for (let source of room.find(FIND_SOURCES))
-    {
-        let site = new HarvestSite(source.id)
-        inst.harvestSites.push(site);
-        siteMap[source.id] = site;
-    }
-
-    // create spawn sites
-    // also add spawn as containers to harvest sites
-    for (let spawn of room.find(FIND_MY_SPAWNS))
-    {
-        let site = new SpawnSite(spawn.id);
-        for (let harv of inst.harvestSites)
-            harv.addContent('container', spawn.id);
-        inst.spawnSites.push(site);
-        siteMap[spawn.id] = site;
-    }
-
-    // add creeps to sites
-    for (let name in Game.creeps)
-    {
-        const creep = Game.creeps[name];
-        if (creep.pos.roomName == inst.roomName && creep.memory['owner'])
-            siteMap[creep.memory['owner']].addContent('creep', creep.id);
-    }
-}
 
 export function loop()
 {
-    // Create room mediators
-    for (let room in Game.rooms)
-        poll(room)
-
-    // Run room mediators
-    for (let room in Game.rooms)
-        RoomMediator.getInstance(room).tick();
-
     // Run Creep funcs
     for(var name in Game.creeps) {
         const creep = Game.creeps[name];
@@ -57,8 +11,4 @@ export function loop()
 
         roles[role](creep);
     }
-
-    // for some reason static objects stick around in the game world??
-    for (let room in Game.rooms)
-        RoomMediators.clearInstance(room);
 }
--- src/sites/funcs/roles/genericRoleHandler.ts
+++ src/sites/funcs/roles/genericRoleHandler.ts
@@ -1,5 +1,13 @@
+import { filter, sortBy } from "lodash";
 import { Site } from "../../Site";
 import RoleHandler from "./RoleHandler";
+
+function assignRole(creep: Creep, role:string, target?:string, initalState=true)
+{
+    creep.memory['role']   = role;
+    creep.memory['target'] = target;
+    creep.memory['state']  = initalState;
+}
 
 function findCollectRole(creep:Creep, site: Site) {
     for (let handler of site.energyRequestHandlers)
@@ -9,7 +17,38 @@ function findCollectRole(creep:Creep, site: Site) {
 
 function findWorkRole(creep: Creep, site: Site)
 {
+    // Fill containers
+    // TODO 
+    //  this will be suboptimal for Storages because of their huge capacity
+    //  some way to limit will be useful
+    let containers = site.objects.getContent('container') as StructureContainer[];
+    containers = filter(containers, x => x.store.getFreeCapacity(RESOURCE_ENERGY) != 0);
+    if (containers.length != 0)
+    {
+        containers = sortBy(containers, x => creep.pos.getRangeTo(x));
+        assignRole(creep, 'fill', containers[0].id);
+        return;
+    }
 
+    // Build Sites
+    let sites = site.objects.getContent('constructionSite') as ConstructionSite[];
+    if (sites.length != 0)
+    {
+        sites = sortBy(sites, x => creep.pos.getRangeTo(x));
+        assignRole(creep, 'build', sites[0].id);
+    }
+
+    // Upgrade controllers
+    let controllers = site.objects.getContent('controller') as StructureController[];
+    controllers = filter(controllers, x=>x.pos.roomName == creep.pos.roomName);
+    if (containers.length != 0)
+    {
+        assignRole(creep, 'upgrade', controllers[0].id);
+        return;
+    }
+
+    // Sit idle :/
+    assignRole(creep, 'idle', null, false);
 }
 
 export default (function (creep:Creep) {
