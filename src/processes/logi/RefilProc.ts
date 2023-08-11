09196545c277dc0173541ef9f6596724a30e8a98
WilliamDann <s-william.dann@lwtech.edu>
2023-08-10 17:07:47 -0700

    filler spawns and uses creeps

--- src/processes/logi/RefilProc.ts
+++ src/processes/logi/RefilProc.ts
@@ -6,6 +6,8 @@ export default class RefilProc extends Process
     memory: {
         roomName : string,
         targets  : Id<_HasId>[],
+
+        creeps   : Id<Creep>[]
     }
 
 
@@ -24,16 +26,43 @@ export default class RefilProc extends Process
     }
 
 
+    // update creeps under the processes control
+    updateCreeps()
+    {
+        this.memory.creeps = [];
+        for (let name in Game.creeps)
+        {
+            const creep = Game.creeps[name];
+            if (creep.memory['owner'] == this.ref)
+                this.memory.creeps.push(creep.id);
+        }
+    }
+
+
+    // run a creep under the processes control
+    runCreep(creep: Creep)
+    {
+        creep.say('filler');
+    }
+
+
     init(): void {
         if (!this.memory.roomName)
             this.memory.roomName = this.name;
 
         if (!this.memory.targets)
             this.memory.targets = [];
+
+        if (!this.memory.creeps)
+            this.updateCreeps();
     }
 
 
     run(): void {
-        // console.log(JSON.stringify(this.memory.targets));
+        if (!this.memory.creeps)
+            this.memory.creeps = [];
+
+        for (let creep of this.memory.creeps)
+            this.runCreep(Game.getObjectById(creep));
     }
 }
